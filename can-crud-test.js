e79b447de5e5216e81f4a708000cafa51f8ee9a8
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-05-10 19:13:52 +0100

    Landscaper: QUnit2 upgrade

--- can-crud-test.js
+++ can-crud-test.js
@@ -3,7 +3,7 @@ import plugin from './can-crud';
 
 QUnit.module('can-crud');
 
-QUnit.test('Initialized the plugin', function(){
-  QUnit.equal(typeof plugin, 'function');
-  QUnit.equal(plugin(), 'This is the can-crud plugin');
+QUnit.test('Initialized the plugin', function(assert) {
+  assert.equal(typeof plugin, 'function');
+  assert.equal(plugin(), 'This is the can-crud plugin');
 });
